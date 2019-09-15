4e561dab94fe5480d315a18a9c919abcff469b7a
Nikolay0951 <egnicholas333@gmail.com>
2019-09-15 21:39:20 +0300

    comment correction

--- bonus.js
+++ bonus.js
@@ -6,6 +6,6 @@ let socks = 169;
 let bonusStep = 1000;
 let bonusSize = 100;
 
-let bonus = Math.floor((bicycle + ball + shorts + socks)/bonusStep)*bonusSize; // Mf-округление значения до целых в меньшую сторону
+let bonus = Math.floor((bicycle + ball + shorts + socks)/bonusStep)*bonusSize; // Mf - rounding down
 
 console.log("Размер бонуса: " + bonus);
